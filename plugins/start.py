97d59289367739234e5da736c24d7b43de668b74
Stardust-Community <arkasarkar235@gmail.com>
2024-11-27 10:51:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -68,7 +68,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Ara Please wait...")
+        temp_msg = await message.reply("<blockquote>Ara Please wait...</blockquote>")
         try:
             messages = await get_messages(client, ids)
         except:
